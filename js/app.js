a312dca9620689bc0b31c9909fe364d81d4e53a0
Chiran <chiran.doshi@childrens.harvard.edu>
2015-07-06 17:25:29 -0400

    added dynamic controller loading using angularAMD

--- js/app.js
+++ js/app.js
@@ -1,49 +1,24 @@
-require.config({
-
-    paths: {
-        app: 'js',
-        angular: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular',
-        angularRoute:'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular-route',
-        
-    },
-    
-    shim: {
-        'angular': {
-            exports: 'angular'
-        },
-        angularRoute: ['angular']
-    }
-
-});
-
-require(['angular','angularRoute'], function (angular) {
+define(['angular', 'angularRoute', 'angularAMD'], function(angular, ngRoute, angularAMD) {
     "use strict";
-        
-    var mainApp = angular.module('mainApp', ['ngRoute']);
-    
-    mainApp.controller('ctrl1', ['$scope' , function ($scope){
-    	$scope.name = 'Hello';
-    }]);
-    
-    mainApp.controller('ctrl2', ['$scope' , function ($scope){
-    	$scope.name = 'Hello';
-    }]);
-    
-    mainApp.config(['$routeProvider',function($routeProvider) {
-        $routeProvider.
-        when('/view1', {
-            templateUrl: 'tpl/view1.html',
-            controller: 'ctrl1'
-        }).
-        when('/view2', {
-            templateUrl: 'tpl/view2.html',
-            controller: 'ctrl2'
-        }).
-        otherwise({
-            redirectTo: '/'
+
+    var app = angular.module('mainApp', ['ngRoute']);
+
+    app.config(['$routeProvider',
+    function($routeProvider) {
+
+        $routeProvider.when('/view1', angularAMD.route({
+            templateUrl : 'views/view1.html',
+            controller : 'ctrl1',
+            controllerUrl : 'controller/ctrl1'
+        })).when('/view2', angularAMD.route({
+            templateUrl : 'views/view2.html',
+            controller : 'ctrl2',
+            controllerUrl : 'controller/ctrl2'
+        })).otherwise({
+            redirectTo : '/'
         });
     }]);
-    
-    angular.bootstrap(document, ['mainApp']);
 
-});
+    return angularAMD.bootstrap(app);
+
+}); 
